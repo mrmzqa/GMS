f5594fd7e28502c85ca0f1abec8170069a2ae54d
mrmzqa <mrmzqa@gmail.com>
2025-07-03 16:11:47 -0700

    rmz411003072025

---
+++ GMS.Models/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMS.Models.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+
+
+
+    }
+}
---
+++ src/GMS25/Pages/Dashboard/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GMS25.Pages.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
