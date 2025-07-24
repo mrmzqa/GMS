0731afb47e103b1dbeb7afc5620a3d900598d07f
mrmzqa <mrmzqa@gmail.com>
2025-07-24 02:25:50 -0700

    Auth##

--- GMS.Models/Models/Employee.cs
+++ GMS.Models/Models/Employee.cs
@@ -12,6 +12,19 @@ namespace GMS.Models.Models
         public string Name { get; set; }
         public string Position { get; set; }
         public decimal Salary { get; set; }
+
+        public string JoiningDate { get; set; }
+
+        public string Department { get; set; }
+
+        public string Email { get; set; }
+
+
+        public string Passportnumber { get; set; }
+
+
+        public string QID { get; set; }
+
         public ICollection<Attendance> Attendances { get; set; }
     }
 }
