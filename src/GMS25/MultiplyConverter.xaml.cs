d76dee543ddf8a587468802ef321e413de652b5a
mrmzqa <mrmzqa@gmail.com>
2025-07-26 01:29:55 -0700

    Update MultiplyConverter.xaml.cs

--- src/GMS25/MultiplyConverter.xaml.cs
+++ src/GMS25/MultiplyConverter.xaml.cs
@@ -21,7 +21,27 @@ namespace GMS25
     {
         public MultiplyConverter()
         {
-            InitializeComponent();
+  
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfPosApp
+{
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is decimal price && values[1] is int quantity)
+            {
+                return price * quantity;
+            }
+            return 0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
