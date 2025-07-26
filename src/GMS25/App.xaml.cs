2f2ac8801e04a73d064a86ad30ae6f04fd1b9608
mrmzqa <mrmzqa@gmail.com>
2025-07-26 01:09:07 -0700

    Update App.xaml.cs

--- src/GMS25/App.xaml.cs
+++ src/GMS25/App.xaml.cs
@@ -1,14 +1,57 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using WpfPosApp.Data;
+using WpfPosApp.Services;
+using WpfPosApp.ViewModels;
+using WpfPosApp.Views;
+using Microsoft.EntityFrameworkCore;
 
-namespace GMS25
+namespace WpfPosApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public static IServiceProvider ServiceProvider { get; private set; }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Initialize database
+            using var dbContext = new AppDbContext();
+            dbContext.Database.Migrate();
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>();
+            
+            services.AddSingleton<IAuthService, AuthService>();
+            services.AddSingleton<IPosService, PosService>();
+            
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<LoginViewModel>();
+            services.AddTransient<HomeViewModel>();
+            services.AddTransient<ProductsViewModel>();
+            services.AddTransient<CartViewModel>();
+            services.AddTransient<OrdersViewModel>();
+            
+            services.AddSingleton<MainWindow>(s => new MainWindow()
+            {
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+            
+            services.AddSingleton<LoginView>();
+            services.AddSingleton<HomeView>();
+            services.AddSingleton<ProductsView>();
+            services.AddSingleton<CartView>();
+            services.AddSingleton<OrdersView>();
+        }
+    }
 }
