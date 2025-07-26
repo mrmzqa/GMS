a78164590249e9974e0ca02db80e653221c4c843
mrmzqa <mrmzqa@gmail.com>
2025-07-26 00:52:10 -0700

    Update Category.cs

--- src/GMS25/Models/Category.cs
+++ src/GMS25/Models/Category.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace GMS25.Models
 {
-    internal class Category
+    public class Category
     {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public ICollection<Product> Products { get; set; }
     }
 }
