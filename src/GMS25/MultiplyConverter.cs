e422204a8d3c6b87757835e16aaaa96500c8eb0b
mrmzqa <mrmzqa@gmail.com>
2025-07-26 01:43:27 -0700

    Update MultiplyConverter.cs

--- src/GMS25/MultiplyConverter.cs
+++ src/GMS25/MultiplyConverter.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
 
-namespace GMS25
+namespace WpfPosApp
 {
-    internal class MultiplyConverter
+    public class MultiplyConverter : IMultiValueConverter
     {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length < 2)
+                return 0;
+
+            decimal result = 1;
+            foreach (var value in values)
+            {
+                if (value is decimal decimalValue)
+                {
+                    result *= decimalValue;
+                }
+                else if (value is int intValue)
+                {
+                    result *= intValue;
+                }
+                else if (value is double doubleValue)
+                {
+                    result *= (decimal)doubleValue;
+                }
+                else if (value is string strValue && decimal.TryParse(strValue, out decimal parsedValue))
+                {
+                    result *= parsedValue;
+                }
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
