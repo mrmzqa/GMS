2fa53b210b9a591ab91eaca32aaee5244b41baf3
mrmzqa <mrmzqa@gmail.com>
2025-07-26 01:13:27 -0700

    Update OrdersViewModel.cs

--- src/GMS25/ViewModels/OrdersViewModel.cs
+++ src/GMS25/ViewModels/OrdersViewModel.cs
@@ -10,3 +10,45 @@ namespace GMS25.ViewModels
     {
     }
 }
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using WpfPosApp.Models;
+using WpfPosApp.Services;
+
+namespace WpfPosApp.ViewModels
+{
+    public partial class OrdersViewModel : ObservableObject
+    {
+        private readonly IPosService _posService;
+
+        [ObservableProperty]
+        private ObservableCollection<Order> _orders;
+
+        [ObservableProperty]
+        private Order _selectedOrder;
+
+        public OrdersViewModel(IPosService posService)
+        {
+            _posService = posService;
+            _ = LoadOrdersAsync();
+        }
+
+        private async Task LoadOrdersAsync()
+        {
+            var orders = await _posService.GetOrdersAsync();
+            Orders = new ObservableCollection<Order>(orders);
+        }
+
+        [RelayCommand]
+        private async Task UpdateOrderStatus(string status)
+        {
+            if (SelectedOrder != null)
+            {
+                await _posService.UpdateOrderStatusAsync(SelectedOrder.OrderId, status);
+                SelectedOrder.Status = status;
+            }
+        }
+    }
+}
