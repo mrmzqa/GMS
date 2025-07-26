16d02dd8db6ca859aca32ec341a54c7434724204
mrmzqa <mrmzqa@gmail.com>
2025-07-26 00:51:11 -0700

    Update Product.cs

--- src/GMS25/Models/Product.cs
+++ src/GMS25/Models/Product.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace GMS25.Models
 {
-    internal class Product
+    public class Product
     {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
+
 }
