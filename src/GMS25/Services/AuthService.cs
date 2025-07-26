5e767f22379b715cf9e09b4c5c53f7176b6cb0ec
mrmzqa <mrmzqa@gmail.com>
2025-07-26 00:59:45 -0700

    Update AuthService.cs

--- src/GMS25/Services/AuthService.cs
+++ src/GMS25/Services/AuthService.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using GMS25.Data;
+using GMS25.Models;
 
 namespace GMS25.Services
 {
-    internal class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly AppDbContext _context;
+        private User _currentUser;
+
+        public bool IsLoggedIn => _currentUser != null;
+        public User CurrentUser => _currentUser;
+
+        public AuthService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            _currentUser = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+            
+            return IsLoggedIn;
+        }
+
+        public void Logout()
+        {
+            _currentUser = null;
+        }
     }
+}
 }
