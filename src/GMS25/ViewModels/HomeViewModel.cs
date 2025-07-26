22bb2d55a824b925b79896dfbd0fd0ba8abaf6ce
mrmzqa <mrmzqa@gmail.com>
2025-07-26 01:16:56 -0700

    Update HomeViewModel.cs

--- src/GMS25/ViewModels/HomeViewModel.cs
+++ src/GMS25/ViewModels/HomeViewModel.cs
@@ -10,3 +10,25 @@ namespace GMS25.ViewModels
     {
     }
 }
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using WpfAuthApp.Services;
+
+namespace WpfAuthApp.ViewModels
+{
+    public partial class HomeViewModel : ObservableObject
+    {
+        private readonly IAuthService _authService;
+
+        public HomeViewModel(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [RelayCommand]
+        private void Logout()
+        {
+            _authService.Logout();
+        }
+    }
+}
