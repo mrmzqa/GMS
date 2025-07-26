0ff779754019c7b1e583fd8daf7b7d4f8d379f3f
mrmzqa <mrmzqa@gmail.com>
2025-07-26 01:24:33 -0700

    Update MainViewModel.cs

--- src/GMS25/ViewModels/MainViewModel.cs
+++ src/GMS25/ViewModels/MainViewModel.cs
@@ -1,36 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GMS25.ViewModels
-{
-    internal class MainViewModel
-    {
-    }
-}
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using WpfAuthApp.Services;
-using WpfAuthApp.Views;
+using WpfPosApp.Services;
+using WpfPosApp.Views;
 
-namespace WpfAuthApp.ViewModels
+namespace WpfPosApp.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
         private readonly IAuthService _authService;
         private readonly LoginView _loginView;
         private readonly HomeView _homeView;
+        private readonly ProductsView _productsView;
+        private readonly CartView _cartView;
+        private readonly OrdersView _ordersView;
 
         [ObservableProperty]
         private object _currentView;
 
-        public MainViewModel(IAuthService authService, LoginView loginView, HomeView homeView)
+        [ObservableProperty]
+        private string _title = "POS System";
+
+        public MainViewModel(
+            IAuthService authService, 
+            LoginView loginView, 
+            HomeView homeView,
+            ProductsView productsView,
+            CartView cartView,
+            OrdersView ordersView)
         {
             _authService = authService;
             _loginView = loginView;
             _homeView = homeView;
+            _productsView = productsView;
+            _cartView = cartView;
+            _ordersView = ordersView;
 
             _authService.Logout(); // Start with logout state
             CurrentView = _loginView;
@@ -48,6 +51,37 @@ namespace WpfAuthApp.ViewModels
         private void UpdateView()
         {
             CurrentView = _authService.IsLoggedIn ? _homeView : _loginView;
+            Title = _authService.IsLoggedIn ? $"POS System - {_authService.CurrentUser.Username}" : "POS System";
+        }
+
+        [RelayCommand]
+        private void NavigateToProducts()
+        {
+            CurrentView = _productsView;
+        }
+
+        [RelayCommand]
+        private void NavigateToCart()
+        {
+            CurrentView = _cartView;
+        }
+
+        [RelayCommand]
+        private void NavigateToOrders()
+        {
+            CurrentView = _ordersView;
+        }
+
+        [RelayCommand]
+        private void NavigateToHome()
+        {
+            CurrentView = _homeView;
+        }
+
+        [RelayCommand]
+        private void Logout()
+        {
+            _authService.Logout();
         }
     }
 }
