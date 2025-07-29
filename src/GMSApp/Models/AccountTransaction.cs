cb5c3db82f67419a14654065b0dba1594368085b
mrmzqa <mrmzqa@gmail.com>
2025-07-29 10:20:24 -0700

    Update AccountTransaction.cs

--- src/GMSApp/Models/AccountTransaction.cs
+++ src/GMSApp/Models/AccountTransaction.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GMSApp.Models
+namespace GarageApp.Models
 {
-    internal class AccountTransaction
+    public enum TransactionType { Receivable, Payable }
+    public enum TransactionStatus { Pending, Paid }
+
+    public class AccountTransaction
     {
+        [Key]
+        public Guid TransactionId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public TransactionType Type { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public TransactionStatus Status { get; set; } = TransactionStatus.Pending;
     }
 }
