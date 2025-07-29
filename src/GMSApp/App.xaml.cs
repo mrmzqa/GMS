556022d756ee0b696823a625546541a15e5e60b4
mrmzqa <mrmzqa@gmail.com>
2025-07-29 07:14:26 -0700

    changes

--- src/GMSApp/App.xaml.cs
+++ src/GMSApp/App.xaml.cs
@@ -1,20 +1,21 @@
 ﻿
+using GMSApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
 public partial class App : Application
 {
-    /*public static IServiceProvider ServiceProvider { get; private set; }
+    public static IServiceProvider ServiceProvider { get; private set; }
 
     protected override void OnStartup(StartupEventArgs e)
     {
         var services = new ServiceCollection();
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer("YourConnectionStringHere"));
+            options.UseSqlServer("DefaultConnection"));
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
         ServiceProvider = services.BuildServiceProvider();
 
         base.OnStartup(e);
-    }*/
+    }
 }
