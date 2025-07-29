53f552a524e3da89dec45797de062fec7a3ca245
mrmzqa <mrmzqa@gmail.com>
2025-07-29 10:21:24 -0700

    Update ServiceJob.cs

--- src/GMSApp/Models/ServiceJob.cs
+++ src/GMSApp/Models/ServiceJob.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GMSApp.Models
+namespace GarageApp.Models
 {
-    internal class ServiceJob
+    public class ServiceJob
     {
+        [Key]
+        public Guid JobId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid VehicleId { get; set; }
+
+        [ForeignKey(nameof(VehicleId))]
+        public Vehicle? Vehicle { get; set; }
+
+        [Required]
+        public string ReportedIssue { get; set; } = string.Empty;
+
+        public string? Diagnosis { get; set; }
+        public string? FixesPerformed { get; set; }
+
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+        public DateTime? EndDate { get; set; }
+
+        public Guid? AssignedWorkerId { get; set; }
+
+        [ForeignKey(nameof(AssignedWorkerId))]
+        public GarageWorker? AssignedWorker { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal Cost { get; set; }
     }
 }
