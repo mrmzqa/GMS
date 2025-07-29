1ba513d20a4bb7f776ac0d90401b34c3108f5012
mrmzqa <mrmzqa@gmail.com>
2025-07-29 10:17:45 -0700

    Update BaseViewModel.cs

--- src/GMSApp/ViewModels/BaseViewModel.cs
+++ src/GMSApp/ViewModels/BaseViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace GMSApp.ViewModels
+namespace GarageApp.ViewModels
 {
-    internal class BaseViewModel
-    {
-    }
+    public class BaseViewModel : ObservableObject
+    { }
 }
