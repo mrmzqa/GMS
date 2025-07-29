2dcb95717f670cdc77a3eaa2c6ef4cdb8fff0e5b
mrmzqa <mrmzqa@gmail.com>
2025-07-29 10:15:50 -0700

    Update NulltoBoolConverter.cs

--- src/GMSApp/Converters/NulltoBoolConverter.cs
+++ src/GMSApp/Converters/NulltoBoolConverter.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Globalization;
+using System.Windows.Data;
 
-namespace GMSApp.Converters
+namespace GarageApp.Converters
 {
-    internal class NulltoBoolConverter
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class NullToBoolConverter : IValueConverter
     {
+        public bool Invert { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isNull = value == null;
+            return Invert ? !isNull : !isNull;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
