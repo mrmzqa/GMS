bc9fbf25be28986974a72335f1a612f762be8d05
mrmzqa <mrmzqa@gmail.com>
2025-07-29 10:14:31 -0700

    Update App.xaml.cs

--- src/GMSApp/App.xaml.cs
+++ src/GMSApp/App.xaml.cs
@@ -1,21 +1,69 @@
-﻿
-using GMSApp.Data;
+using GarageApp.Data;
+using GarageApp.Services;
+using GarageApp.ViewModels;
+using GarageApp.Views;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 using System.Windows;
 
-public partial class App : Application
+namespace GarageApp
 {
-    public static IServiceProvider ServiceProvider { get; private set; }
-
-    protected override void OnStartup(StartupEventArgs e)
+    public partial class App : Application
     {
-        var services = new ServiceCollection();
-        services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer("DefaultConnection"));
-        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-        ServiceProvider = services.BuildServiceProvider();
+        private readonly IHost _host;
 
-        base.OnStartup(e);
+        public App()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs\\garage.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDbContext<GarageDbContext>(options =>
+                        options.UseSqlite("Data Source=garage.db"));
+
+                    services.AddScoped<AuthenticationService>();
+
+                    services.AddTransient<LoginViewModel>();
+                    services.AddTransient<VehicleViewModel>();
+
+                    services.AddTransient<MainWindowViewModel>();
+
+                    // Views
+                    services.AddSingleton<MainWindow>();
+                    services.AddTransient<LoginView>();
+                    services.AddTransient<VehiclesPage>();
+                    services.AddTransient<MainContentView>();
+                })
+                .UseSerilog()
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            Log.Information("Application starting");
+
+            await _host.StartAsync();
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            var mainVm = _host.Services.GetRequiredService<MainWindowViewModel>();
+            mainWindow.DataContext = mainVm;
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            Log.Information("Application exiting");
+            await _host.StopAsync();
+            Log.CloseAndFlush();
+            base.OnExit(e);
+        }
     }
 }
