7df9ea0097c4b9600b0adaf3c5e6d3c9237f6a9c
mrmzqa <mrmzqa@gmail.com>
2025-07-29 10:20:43 -0700

    Update PaymentReceipt.cs

--- src/GMSApp/Models/PaymentReceipt.cs
+++ src/GMSApp/Models/PaymentReceipt.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GMSApp.Models
+namespace GarageApp.Models
 {
-    internal class PaymentReceipt
+    public class PaymentReceipt
     {
+        [Key]
+        public Guid ReceiptId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid JobId { get; set; }
+
+        [ForeignKey(nameof(JobId))]
+        public ServiceJob? ServiceJob { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal AmountPaid { get; set; }
+
+        public string PaymentMethod { get; set; } = string.Empty;
+
+        public DateTime DatePaid { get; set; } = DateTime.UtcNow;
+
+        public string? Remarks { get; set; }
     }
 }
