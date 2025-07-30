ef38f1f9597c254dc1ec23a83fceb3ff40ee2f0b
mrmzqa <mrmzqa@gmail.com>
2025-07-30 06:16:24 -0700

    changes

--- src/GMSApp/Data/GarageDbContext.cs
+++ src/GMSApp/Data/GarageDbContext.cs
@@ -38,7 +38,7 @@ namespace GMSApp.Data
 
             var superAdminUser = new User
             {
-                UserId = Guid.NewGuid(),
+                UserId = Guid.Parse("123e4567-e89b-12d3-a456-426614174000"),
                 Username = "superadmin",
                 PasswordHash = superAdminPasswordHash,
                 Role = UserRole.SuperAdmin,
