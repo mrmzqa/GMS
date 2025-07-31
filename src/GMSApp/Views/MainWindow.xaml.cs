fe219a84fbc942d67f8500ff16811f612ed1057e
mrmzqa <mrmzqa@gmail.com>
2025-07-31 08:01:48 -0700

    Update MainWindow.xaml.cs

--- src/GMSApp/Views/MainWindow.xaml.cs
+++ src/GMSApp/Views/MainWindow.xaml.cs
@@ -7,11 +7,14 @@ namespace GMSApp.Views
     public partial class MainWindow : Window
     {
         private readonly FilesPage _filesPage;
+        private readonly VehiclePage _vehiclePage;
 
-        public MainWindow(FilesPage filesPage)
+        public MainWindow(FilesPage filesPage , VehiclePage vehiclePage)
         {
             InitializeComponent();
             _filesPage = filesPage;
+            _vehiclePage = vehiclePage;
+
         }
 
         private void FilesButton_Click(object sender, RoutedEventArgs e)
