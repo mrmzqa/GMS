37ec8ea6497acbc330af08d557784dcde78603ba
mrmzqa <mrmzqa@gmail.com>
2025-08-02 08:56:38 -0700

    Update CoreMain.cs

--- src/GMSApp/Models/CoreMain.cs
+++ src/GMSApp/Models/CoreMain.cs
@@ -8,14 +8,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GMSApp.Models
 {
+   
     public class CoreMain
     {
         [Key]
         public int Id { get; set; }
 
-        [Required, MaxLength(200)]
+        [Required]
         public string Name { get; set; } = string.Empty;
 
-        public virtual ICollection<Main> Mains { get; set; } = new List<Main>();
+        public byte[]? HeaderFile { get; set; }
+        public string? HeaderName { get; set; }
+
+        public byte[]? FooterFile { get; set; }
+        public string? FooterName { get; set; }
+
+        public virtual ICollection<Main> Main { get; set; } = new List<Main>();
     }
+
 }
