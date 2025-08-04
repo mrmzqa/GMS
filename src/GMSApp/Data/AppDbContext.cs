2b8b27c52a4e96f3e047900ddf110c2e3b068e38
mrmzqa <mrmzqa@gmail.com>
2025-08-04 12:14:54 -0700

    Update AppDbContext.cs

--- src/GMSApp/Data/AppDbContext.cs
+++ src/GMSApp/Data/AppDbContext.cs
@@ -13,6 +13,8 @@ namespace GMSApp.Data
         public DbSet<FileItem> Files { get; set; }
         public DbSet<CoreMain> CoreMains { get; set; }
         public DbSet<Main> Mains { get; set; }
+public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
