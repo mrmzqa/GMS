e93a883aea21fb5f9f24607bce3e318f93218ed9
mrmzqa <mrmzqa@gmail.com>
2025-08-05 09:08:34 -0700

    changes

---
+++ src/GMSApp/Views/Inventory/InventoryCategory.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GMSApp.Views.Inventory
+{
+    /// <summary>
+    /// Interaction logic for InventoryCategory.xaml
+    /// </summary>
+    public partial class InventoryCategory : UserControl
+    {
+        public InventoryCategory()
+        {
+            InitializeComponent();
+        }
+    }
+}
