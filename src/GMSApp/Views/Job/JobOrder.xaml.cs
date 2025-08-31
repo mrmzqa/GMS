3e89d6a5314dc265b07b6bc2ffed27d045e2b008
mrmzqa <mrmzqa@gmail.com>
2025-09-01 01:51:18 +0300

    changes

--- src/GMSApp/Views/Job/JobOrder.xaml.cs
+++ src/GMSApp/Views/Job/JobOrder.xaml.cs
@@ -1,31 +1,18 @@
-﻿using GMSApp.ViewModels.Job;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿
+
+using GMSApp.ViewModels.Job;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GMSApp.Views.Job
 {
-
     public partial class JobOrder : UserControl
     {
-        // Parameterless ctor for designer support
         public JobOrder()
         {
             InitializeComponent();
         }
 
-        // Use this constructor when composing from DI (e.g. in a window or a view locator)
+        // Use this ctor when resolving the VM via DI so DataContext is set automatically.
         public JobOrder(JoborderViewModel viewModel) : this()
         {
             DataContext = viewModel;
