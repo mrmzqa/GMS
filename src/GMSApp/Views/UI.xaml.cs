cfcb6e429da2ebec1d2e714ca32873efdb2f6b33
mrmzqa <mrmzqa@gmail.com>
2025-09-01 01:34:24 +0300

    Update UI.xaml.cs

--- src/GMSApp/Views/UI.xaml.cs
+++ src/GMSApp/Views/UI.xaml.cs
@@ -162,3 +162,23 @@
         </Grid>
     </Grid>
 </UserControl>
+
+using GMSApp.ViewModels.Job;
+using System.Windows.Controls;
+
+namespace GMSApp.Views.Job
+{
+    public partial class JobOrder : UserControl
+    {
+        public JobOrder()
+        {
+            InitializeComponent();
+        }
+
+        // Use this ctor when resolving the VM via DI so DataContext is set automatically.
+        public JobOrder(JoborderViewModel viewModel) : this()
+        {
+            DataContext = viewModel;
+        }
+    }
+}
