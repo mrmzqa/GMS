9d1e904376453fc5cc5a37e83a16f72d7ae7ea2a
rogart <mrmzqa@gmail.com>
2025-09-03 23:52:33 +0300

    changes

---
+++ src/GMSApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMSApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } =  string.Empty ;
+
+
+        public bool IsAuthenticated { get; set; }
+    }
+}
